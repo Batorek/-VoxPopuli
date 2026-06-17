254e443e0970e8aa6e546b2f05bb2fb139843f20
Natis2601 <natalia.galuszczynska0126@gmail.com>
2026-06-18 01:48:02 +0200

    blokada przed podwojnym glosowaniem

--- backend/surveys/views.py
+++ backend/surveys/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, permissions
+from rest_framework.exceptions import ValidationError
 from .models import Ankieta, Odpowiedz
 from .serializers import AnkietaSerializer, OdpowiedzSerializer
 
@@ -14,3 +15,17 @@ class OdpowiedzViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Odpowiedz.objects.filter(pytanie__ankieta_id=self.kwargs.get('ankieta_pk'))
+    
+    def perform_create(self, serializer):
+        pytanie = serializer.validated_data['pytanie']
+        uzytkownik = self.request.user
+
+        
+        czy_glosowal = Odpowiedz.objects.filter(pytanie=pytanie, id_usera=uzytkownik.id).exists()
+        
+        if czy_glosowal:
+            
+            raise ValidationError({"error_code": "VOTE_ALREADY_EXISTS"})
+
+       
+        serializer.save(id_usera=uzytkownik.id)
